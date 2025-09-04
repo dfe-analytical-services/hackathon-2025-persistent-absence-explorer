ee90f633a9c382047075ad7f622aae143c296d96
WELLER <Matt1.WELLER@EDUCATION.GOV.UK>
2025-09-04 13:35:38 +0100

    Fix removal of weekly data

--- R/read_data_reasons.R
+++ R/read_data_reasons.R
@@ -1,19 +1,19 @@
-library(data.table, pos = 1)
-library(dplyr)
-
-reasons_ytd <- fread("./data/attendance_data_dashboard/EES_ytd_data.csv")
-
-reasons_ytd <- reasons_ytd %>%
-  mutate(geo_breakdown = case_when(
-    geographic_level == "National" ~ "National", # NA_character_,
-    geographic_level == "Regional" ~ region_name,
-    geographic_level == "Local authority" ~ la_name
-  ))
-
-reasons_ytd <- reasons_ytd[, .(geographic_level, geo_breakdown)]
-
-reasons_weekly <- fread("./data/attendance_data_dashboard/EES_weekly_data.csv")
-reasons_weekly[, week_number := sapply(time_identifier, function(x) as.numeric(gsub("Week ", "", x)))]
-latest_week <- reasons_weekly[, .(week_number = max(week_number)), by = time_period]
-
-head(reasons_weekly, 3)
+# library(data.table, pos = 1)
+# library(dplyr)
+#
+# reasons_ytd <- fread("./data/attendance_data_dashboard/EES_ytd_data.csv")
+#
+# reasons_ytd <- reasons_ytd %>%
+#   mutate(geo_breakdown = case_when(
+#     geographic_level == "National" ~ "National", # NA_character_,
+#     geographic_level == "Regional" ~ region_name,
+#     geographic_level == "Local authority" ~ la_name
+#   ))
+#
+# reasons_ytd <- reasons_ytd[, .(geographic_level, geo_breakdown)]
+#
+# reasons_weekly <- fread("./data/attendance_data_dashboard/EES_weekly_data.csv")
+# reasons_weekly[, week_number := sapply(time_identifier, function(x) as.numeric(gsub("Week ", "", x)))]
+# latest_week <- reasons_weekly[, .(week_number = max(week_number)), by = time_period]
+#
+# head(reasons_weekly, 3)
